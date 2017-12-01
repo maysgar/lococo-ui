ff8890bc1161585986c94a6408c3dc8fd477f9d1
maysgar <100346215@alumnos.uc3m.es>
2017-12-01 18:19:22 +0100

    boom boom pow

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,9 @@
-//mucha mierda loco
+
+
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+  });
+
+  // $(document).ready(function(){
+  //   $('ul.tabs').tabs('select_tab', 'tab_id');
+  // });
